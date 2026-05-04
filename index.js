60e8da2f835c3ce660d99e94d4b242ec7ae84d50
POUZET Theo <theo.pouzet@maif.fr>
2026-05-04 10:41:08 +0200

    Mon premier validateur d'email

---
+++ index.js
@@ -0,0 +1,21 @@
+import validator from 'validator';
+import chalk from 'chalk';
+
+function validateEmail(email) {
+
+    const isValid = validator.isEmail(email);
+    if (isValid) {
+        console.log(chalk.green('${email} est valide'))
+    } else {
+        console.log(chalk.red('${email} est mal formaté'))
+    }
+}
+const emailsToTest = [
+    'test@example.com',
+    'pas-un-email.fr',
+    'user.name@domain.co',
+    'mauvais@format',
+    'hello@world.io'
+];
+
+emailsToTest.forEach(email => validateEmail(email));
